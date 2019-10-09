61750337e6ee27fa1a6aa7d1c92baf2c4bcbabb3
Frank Acquaye <acquayefrank@gmail.com>
2019-10-09 03:44:50 +0300

    full stackoverflow developer

--- bool.py
+++ bool.py
@@ -1,149 +1,169 @@
-import sys
-
 import ply.lex as lex
 import ply.yacc as yacc
 
+values_of_variables=[]
 
-tokens = ['CONJUNCTION', 'DISJUNCTION', 'IMPLICATION', 'VAR']
-literals = ['(', ')', '~']
+tokens = [
+    'VAR',
+    'IMPLICATION',
+    'CONJUNCTION',
+    'DISJUNCTION',
+    'LPAREN',
+    'RPAREN',
+]
 
+states = (
+    ('negation', 'exclusive'),
+)
 
+t_IMPLICATION = r'->'
 t_CONJUNCTION = r'/\\'
 t_DISJUNCTION = r'\\/'
-t_IMPLICATION = r'->'
-t_VAR = r'[a-z]'
-t_ignore = " \t"
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
 
+def t_NEG(t):
+    r'~'
+    t.lexer.begin('negation')
 
-clauses = []
+def t_negation_VAR(t):
+    r'[a-z]'
+    val = t.value
+    if val in values_of_variables:
+        t.value = values_of_variables.index(val) + 1
+    else:
+        values_of_variables.append(val)
+        t.value = len(values_of_variables)
+    t.value*=(-1)
+    t.lexer.begin('INITIAL')
+    return t
 
-def _clean_clause(p):
-    return [ x for x in p if x ]
+def t_VAR(t):
+    r'[a-z]'
+    val=t.value
+    if val in values_of_variables:
+        t.value=values_of_variables.index(val)+1
+    else:
+        values_of_variables.append(val)
+        t.value = len(values_of_variables)
+    return t
 
 def t_newline(t):
     r'\n+'
-    t.lexer.lineno += t.value.count("\n")
+    t.lexer.lineno += len(t.value)
 
+t_negation_ignore = " \t\n"
+
+t_ignore  = ' \t'
+
+def t_negation_error(t):
+    print('Lexical error: "' + str(t.value[0]) + '" in line ' + str(t.lineno))
+    t.lexer.skip(1)
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
-
-# Build the lexer
-lex.lex()
+lexer = lex.lex()
 
 
-def p_formular(p):
-    """formular : var
-               | disjunctive_formular
-               | conjunctive_formular
-               | implicative_formular
-               | negative_formular
-               | '(' formular ')'
-               """
+clauses=[]
 
+'''
+Fm      ::= Fm CONJUNCTION Clause | Clause  
+Clause  ::= LPAREN Var IMPLICATION Var RPAREN |
+            LPAREN Var DISJUNCTION Var RPAREN |
+            Var
+'''
 
-def p_var(p):
-    """ var : VAR """
-    clauses.append(_clean_clause(p))
-
-
-def p_disjunctive_formular(p):
-    """
-    disjunctive_formular : paren_disjunctive_formular
-                         | no_paren_disjunctive_formular
-    """
-
-
-def p_paren_disjunctive_formular(p):
-    """
-    paren_disjunctive_formular : '(' formular DISJUNCTION formular ')'
-    """
-    clauses.append(_clean_clause(p))
-
-
-def p_no_paren_disjunctive_formular(p):
-    """
-    no_paren_disjunctive_formular : formular DISJUNCTION formular 
-    """
-    clauses.append(_clean_clause(p))
-
-
-def p_conjunctive_formular(p):
-    """
-    conjunctive_formular :  paren_conjunctive_formular
-                         |  no_paren_conjunctive_formular 
-    """
-
-
-def p_paren_conjunctive_formular(p):
-    """
-    paren_conjunctive_formular : '(' formular CONJUNCTION formular ')'
-    """
-    clauses.append(_clean_clause(p))
+def p_Fm_conj(p):
+    'Fm : Fm CONJUNCTION Clause'
     clauses.append(p[3])
 
-def p_no_paren_conjunctive_formular(p):
-    """
-    no_paren_conjunctive_formular : formular CONJUNCTION formular 
-    """
-    clauses.append(_clean_clause(p))
+def p_Fm_term(p):
+    'Fm : Clause'
+    clauses.append(p[1])
 
+def p_Clause_impl(p):
+    'Clause : LPAREN Var IMPLICATION Var RPAREN'
+    p[0]=((-1)*p[2], p[4])
 
-def p_implicative_formular(p):
-    """
-    implicative_formular : paren_implicative_formular
-                         |  no_paren_implicative_formular
-    """
+def p_term_disj(p):
+    'Clause : LPAREN Var DISJUNCTION Var RPAREN'
+    p[0]=(p[2] , p[4])
 
+def p_term_literal(p):
+    'Clause : Var'
+    p[0]=(p[1],0)
 
-def p_paren_implicative_formular(p):
-    """
-    paren_implicative_formular : '(' formular IMPLICATION formular ')'
-    """
-    clauses.append(_clean_clause(p))
-
-def p_no_paren_implicative_formular(p):
-    """
-    no_paren_implicative_formular : formular IMPLICATION formular
-    """
-    clauses.append(_clean_clause(p))
-
-
-def p_negative_formular(p):
-    """
-    negative_formular : no_paren_negative_formular
-    """
-
-
-def p_no_paren_negative_formular(p):
-    """
-    no_paren_negative_formular : '~' formular 
-    """
-    clauses.append(_clean_clause(p))
-
+def p_literal_simple(p):
+    'Var : VAR'
+    p[0] = p[1]
 
 def p_error(p):
-    try:
-        print("Syntax error at '%s'" % p.value)
-    except Exception:
-        print("Hmmm, you're doing something funky, call the guy who wrote the code")
-        sys.exit()
+    message = """
+                Syntax error!
+                Ensure you follow this grammar: 
+                Fm      ::= Fm /\ Clause | Clause")
+                Clause  ::= ( Var -> Var ) |")
+                            ( Var \/ Var ) |")
+                            Var"
+                Var     ::= literal | ~ literal
+            """
+    print(message)
+    raise SyntaxError("error in input!")
+
+def find_resolution(el1, el2):
+    for i in range(2):
+        for j in range(2):
+            if el1[i]!=0 and el2[j]!=0 and el1[i]+el2[j]==0:
+                new_pair=(el1[(i+1)%2],el2[(j+1)%2])
+                if new_pair[0] != 0 and new_pair[1] != 0 and new_pair[0] + new_pair[1] == 0:
+                    return (0,0)
+                return new_pair
+    return 0
 
 
-yacc.yacc()
+def resolution(left_bound, arr):
+    init_size=len(arr)
+    for i in range(left_bound, init_size):
+        elem=arr[i]
+        for j in range(0,left_bound):
+            new_elem=find_resolution(elem, arr[j])
+            if new_elem==0:
+                continue
+            if new_elem == (0,0):
+                return -1,arr
+            else:
+                arr.append(new_elem)
+    return init_size, arr
+
+def launch(clauses):
+    left_bound=1
+    while left_bound!=len(clauses):
+        left_bound,clauses=resolution(left_bound,clauses)
+        if left_bound== -1:
+            print("\n\t\tunsatisfiable")
+            return False
+    print("\n\t\tsatisfiable")
+    return True
 
 
 if __name__ == '__main__':
+    parser = yacc.yacc()
     while True:
         try:
-            s = input('bool supports 2CNF only > ')
+            s = input('\nbool (Works with 2CNF) > ')
             if s == 'exit':
                 break
         except EOFError:
             break
         if not s: continue
-        clauses.clear() # a work around for side effects
-        yacc.parse(s)
-        print(clauses)
+
+        try:
+            result = parser.parse(s)
+            res=launch(clauses)
+        except SyntaxError:
+            continue
+        clauses.clear() # Used to fix sideeffects
+
